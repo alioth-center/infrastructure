94103524d315c6904ce4209b0f8aef8127251295
sunist-c <sunist.c@outlook.com>
2024-02-04 11:22:13 +0800

    optimize: 清理了rpc包中部分无效代码

--- network/rpc/service.go
+++ network/rpc/service.go
@@ -2,10 +2,6 @@ package rpc
 
 import "google.golang.org/grpc"
 
-type ServiceMetadata struct {
-	ServiceName string
-}
-
 type Service interface {
 	Initialization()
 	BindEngine(conn *grpc.Server)
