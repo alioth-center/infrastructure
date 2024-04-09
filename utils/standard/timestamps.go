3f811437137da0af5261cc3557ae6b0227ec7f9b
sunist-c <sunist.c@outlook.com>
2024-04-09 09:08:32 +0800

    update: 为时间戳标准添加注释

--- utils/standard/timestamps.go
+++ utils/standard/timestamps.go
@@ -12,24 +12,52 @@ package standard
 type TimestampFormat = string
 
 const (
-	// TimestampFormatRFC822 RFC 822 时间格式，更新于RFC 1123以允许4位年份
+	// TimestampFormatRFC822 RFC 822 time format, see [RFC 822]
+	//
+	// example: Mon, 02 Jan 06 15:04:05 MST
+	//
+	// [RFC 822]: https://datatracker.ietf.org/doc/html/rfc822
 	TimestampFormatRFC822 TimestampFormat = "Mon, 02 Jan 06 15:04:05 MST"
 
-	// TimestampFormatRFC850 RFC 850 时间格式，后来被RFC 1036取代
+	// TimestampFormatRFC850 RFC 850 time format, see [RFC 850]
+	//
+	// example: Monday, 02-Jan-06 15:04:05 MST
+	//
+	// [RFC 850]: https://datatracker.ietf.org/doc/html/rfc850
 	TimestampFormatRFC850 TimestampFormat = "Monday, 02-Jan-06 15:04:05 MST"
 
-	// TimestampFormatRFC1036 RFC 1036 时间格式，更新了RFC 850的时间格式
+	// TimestampFormatRFC1036 RFC 1036 time format, see [RFC 1036]
+	//
+	// example: Mon, 02 Jan 06 15:04:05 MST
+	//
+	// [RFC 1036]: https://datatracker.ietf.org/doc/html/rfc1036
 	TimestampFormatRFC1036 TimestampFormat = "Mon, 02 Jan 06 15:04:05 MST"
 
-	// TimestampFormatRFC1123 RFC 1123 时间格式，基于RFC 822，广泛用于HTTP和其他互联网协议
+	// TimestampFormatRFC1123 RFC 1123 time format, see [RFC 1123]
+	//
+	// example: Mon, 02 Jan 2006 15:04:05 MST
+	//
+	// [RFC 1123]: https://datatracker.ietf.org/doc/html/rfc1123
 	TimestampFormatRFC1123 TimestampFormat = "Mon, 02 Jan 2006 15:04:05 MST"
 
-	// TimestampFormatRFC2822 RFC 2822 时间格式，用于电子邮件，是RFC 822的后继
+	// TimestampFormatRFC2822 RFC 2822 time format, see [RFC 2822]
+	//
+	// example: Mon, 02 Jan 2006 15:04:05 -0700
+	//
+	// [RFC 2822]: https://datatracker.ietf.org/doc/html/rfc2822
 	TimestampFormatRFC2822 TimestampFormat = "Mon, 02 Jan 2006 15:04:05 -0700"
 
-	// TimestampFormatRFC3339 RFC 3339 时间格式，基于ISO 8601，用于互联网时间戳
+	// TimestampFormatRFC3339 RFC 3339 time format, see [RFC 3339]
+	//
+	// example: 2006-01-02T15:04:05Z07:00
+	//
+	// [RFC 3339]: https://datatracker.ietf.org/doc/html/rfc3339
 	TimestampFormatRFC3339 TimestampFormat = "2006-01-02T15:04:05Z07:00"
 
-	// TimestampFormatRFC5322 RFC 5322 时间格式，更新了RFC 2822，用于电子邮件
+	// TimestampFormatRFC5322 RFC 5322 time format, see [RFC 5322]
+	//
+	// example: Mon, 02 Jan 2006 15:04:05 -0700
+	//
+	// [RFC 5322]: https://datatracker.ietf.org/doc/html/rfc5322
 	TimestampFormatRFC5322 TimestampFormat = "Mon, 02 Jan 2006 15:04:05 -0700"
 )
