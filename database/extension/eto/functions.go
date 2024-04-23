dca76e38440aec2624f60c4075128bee93e45179
sunist-c <sunist.c@outlook.com>
2024-04-23 17:10:04 +0800

    delete: 去除eto相关实现

--- database/extension/eto/functions.go
+++
@@ -1,9 +0,0 @@
-package eto
-
-import "github.com/alioth-center/infrastructure/database"
-
-func DtoGetOne[po, dto any](db database.Database, model po, condition dto) (result dto, err error) {
-	gorm := db.ExtMethods().GetGorm()
-	err = gorm.Model(model).First(condition).Scan(result).Error
-	return result, err
-}
