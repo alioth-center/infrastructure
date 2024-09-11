71e3a99999fa5ecaceb35ebc193fed131ffd9a4a
sunist-c <sunist.c@outlook.com>
2024-09-11 16:50:26 +0800

    feat(utils):  添加判断 ip/cidr 是否合法，判断 ip 是否在 cidr 内的方法

--- utils/network/ip.go
+++ utils/network/ip.go
@@ -57,6 +57,52 @@ func IsPrivateIP(ip string) (isPrivate bool) {
 //	isValid (bool): Returns true if the IP address is a valid IP address,
 //	                false otherwise.
 func IsValidIP(ip string) (isValid bool) {
-	// Parse string ip to net.IP, if it's not nil, it's a valid IP
+	// Parse string ip to net.IP, if err is nil, it's a valid IP
 	return net.ParseIP(ip) != nil
+}
+
+// IsValidIPOrCIDR determines whether a given string is a valid IP address or CIDR.
+// It first parses the string as an IP address, if it fails, it then parses the string as a CIDR.
+// If both parsing operations fail, the string is not a valid IP address or CIDR.
+//
+// Parameters:
+//
+//	ipOrCIDR (string): The IP address or CIDR in string format to be checked.
+//
+// Returns:
+//
+//	isValid (bool): Returns true if the string is a valid IP address or CIDR,
+//	                false otherwise.
+func IsValidIPOrCIDR(ipOrCIDR string) (isValid bool) {
+	// Parse string ip to net.IP, if error is nil, it's a valid IP
+	if net.ParseIP(ipOrCIDR) != nil {
+		return true
+	}
+
+	// Parse string ip to net.IPNet, if error is nil, it's a valid CIDR
+	_, _, err := net.ParseCIDR(ipOrCIDR)
+	return err == nil
+}
+
+// IPInCIDR determines whether a given IP address is in a given CIDR.
+// It parses the IP address and CIDR from strings and checks if the IP address is in the CIDR.
+//
+// Parameters:
+//
+//	ip (string): The IP address in string format to be checked.
+//	cidr (string): The CIDR in string format to be checked.
+//
+// Returns:
+//
+//	inCIDR (bool): Returns true if the IP address is in the CIDR, false otherwise.
+func IPInCIDR(ip, cidr string) (inCIDR bool) {
+	// Parse IP and CIDR, if error is not nil, return false
+	addr := net.ParseIP(ip)
+	_, ipNet, err := net.ParseCIDR(cidr)
+	if addr == nil || err != nil {
+		return false
+	}
+
+	// Check if IP is in CIDR
+	return ipNet.Contains(addr)
 }
--- utils/network/unit_test.go
+++ utils/network/unit_test.go
@@ -57,7 +57,7 @@ func TestIsPrivateIP(t *testing.T) {
 	}
 }
 
-func TestIsVaildIP(t *testing.T) {
+func TestIsValidIP(t *testing.T) {
 	type args struct {
 		ip string
 	}
@@ -96,11 +96,143 @@ func TestIsVaildIP(t *testing.T) {
 			args: args{ip: ""},
 			want: false,
 		},
+		{
+			name: "ip with mask",
+			args: args{ip: "192.168.1.10/24"},
+			want: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := IsValidIP(tt.args.ip); got != tt.want {
 				t.Errorf("IsValidIP() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsValidIPOrCIDR(t *testing.T) {
+	tests := []struct {
+		name string
+		ip   string
+		want bool
+	}{
+		{
+			name: "valid ip 1",
+			ip:   "1.1.1.1",
+			want: true,
+		},
+		{
+			name: "valid ip 2",
+			ip:   "192.168.1.1",
+			want: true,
+		},
+		{
+			name: "valid ipv6",
+			ip:   "2001:4860:4860::8888",
+			want: true,
+		},
+		{
+			name: "invalid ip",
+			ip:   "999.999.999.999",
+			want: false,
+		},
+		{
+			name: "invalid ipv6",
+			ip:   "2001:4860:4860::8888:",
+			want: false,
+		},
+		{
+			name: "empty ip",
+			ip:   "",
+			want: false,
+		},
+		{
+			name: "cidr valid",
+			ip:   "192.168.1.10/24",
+			want: true,
+		},
+		{
+			name: "cidr invalid",
+			ip:   "192.168.1.10/33",
+			want: false,
+		},
+		{
+			name: "ipv6 cidr valid",
+			ip:   "2001:4860:4860::8888/64",
+			want: true,
+		},
+		{
+			name: "ipv6 cidr invalid",
+			ip:   "2001:4860:4860::8888/129",
+			want: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidIPOrCIDR(tt.ip); got != tt.want {
+				t.Errorf("IsValidIPOrCIDR() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIPInCIDR(t *testing.T) {
+	type args struct {
+		ip   string
+		cidr string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "ip in cidr",
+			args: args{cidr: "192.168.1.0/24", ip: "192.168.1.25"},
+			want: true,
+		},
+		{
+			name: "ip not in cidr",
+			args: args{cidr: "192.168.1.0/24", ip: "192.168.2.1"},
+			want: false,
+		},
+		{
+			name: "ipv6 ip in cidr",
+			args: args{cidr: "2001:db8::/32", ip: "2001:db8:0:1::1"},
+			want: true,
+		},
+		{
+			name: "ipv6 ip not in cidr",
+			args: args{cidr: "2001:db8::/32", ip: "2001:db9::1"},
+			want: false,
+		},
+		{
+			name: "another ipv6 ip in cidr",
+			args: args{cidr: "fd00::/8", ip: "fd12:3456:789a:1::1"},
+			want: true,
+		},
+		{
+			name: "another ipv6 ip not in cidr",
+			args: args{cidr: "fd00::/8", ip: "fe80::1"},
+			want: false,
+		},
+		{
+			name: "invalid ip",
+			args: args{cidr: "192.168.1.0/24", ip: "192.168.1.256"},
+			want: false,
+		},
+		{
+			name: "invalid cidr",
+			args: args{cidr: "192.168.1.0/64", ip: "192.168.1.1"},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IPInCIDR(tt.args.ip, tt.args.cidr); got != tt.want {
+				t.Errorf("isIPInCIDR() = %v, want %v", got, tt.want)
 			}
 		})
 	}
